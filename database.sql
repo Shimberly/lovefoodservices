37451d1af6551592b241dec685f98a3ea3b7b293
Shimberly <kimmy_11_07@hotmail.com>
2019-01-20 19:58:35 -0500

    Create de informacion y Inserts de prueba

--- database.sql
+++ database.sql
@@ -37,3 +37,30 @@ CREATE TABLE respuesta(
       REFERENCES   usuario  ( id ));
 );
 
+CREATE TABLE informacion(
+  id serial NOT NULL,
+  idusuario integer,
+  universidad varchar(255),
+  ciudad varchar(255),
+  pais varchar(255),
+  carrera varchar(255),
+  PRIMARY KEY (id),
+  CONSTRAINT  idusuario 
+      FOREIGN KEY ( idusuario )
+      REFERENCES   usuario  ( id ));
+);
+
+INSERT INTO usuario ( nombre,apellido,numero,correo,clave,sexo,fecha_nacimiento) VALUES
+('Webbi','SA','0985992827','webbisolutions@gmail.com','teamwebbi','mujer','04-05-2018');
+
+INSERT INTO usuario ( nombre,apellido,numero,correo,clave,sexo,fecha_nacimiento) VALUES
+('Paola','Ortiz','0985992827','giiina.paola@gmail.com','jagger123','mujer','21-02-1996');
+
+INSERT INTO usuario ( nombre,apellido,numero,correo,clave,sexo,fecha_nacimiento) VALUES
+('Kimmy','Munoz','0985992827','lakimmy@gmail.com','rayito123','mujer','07-09-1996');
+
+INSERT INTO respuesta (idusuario,rspPreferencia,rspVerde,rspDesayunoSalado,rspDesayunoDulce,rspAlmuerzo,rspMarisco,rspSopa,rspCena,rspComidaTipicaCosta,rspComidaTipicaSierra,rspComidaTipicaOriente,rspProteina,rspPostres,rspSaboresDulces,rspBebida,rspComidaExtranjera,rspComidaRapida) VALUES (1,'hombre','bolon','verde','panqueque','seco de pollo','ninguno','sopa de queso','carnes','arroz con menestra','fritada','ninguna','pollo','helado','chocolate','jugo','japonesa','salchipapa');
+INSERT INTO respuesta (idusuario,rspPreferencia,rspVerde,rspDesayunoSalado,rspDesayunoDulce,rspAlmuerzo,rspMarisco,rspSopa,rspCena,rspComidaTipicaCosta,rspComidaTipicaSierra,rspComidaTipicaOriente,rspProteina,rspPostres,rspSaboresDulces,rspBebida,rspComidaExtranjera,rspComidaRapida) VALUES (1,'hombre','bolon','verde','panqueque','seco de pollo','ninguno','sopa de queso','carnes','arroz con menestra','fritada','ninguna','pollo','helado','chocolate','jugo','japonesa','salchipapa');
+
+INSERT INTO informacion (idusuario,universidad,ciudad,pais,carrera) VALUES
+(2,'ESPOL','Guayaquil','Ecuador','Diseño web y aplicaciones multimedia');
